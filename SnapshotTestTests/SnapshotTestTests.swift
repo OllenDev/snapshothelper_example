744d1c1ce60c8077fabaaa4f10d41402aedb8171
Chris Ollenburg <chris.ollenburg@skookum.com>
2016-03-09 10:47:20 -0500

    Initial Commit

---
+++ SnapshotTestTests/SnapshotTestTests.swift
@@ -0,0 +1,36 @@
+//
+//  SnapshotTestTests.swift
+//  SnapshotTestTests
+//
+//  Created by Chris Ollenburg on 3/9/16.
+//  Copyright © 2016 ollendev. All rights reserved.
+//
+
+import XCTest
+@testable import SnapshotTest
+
+class SnapshotTestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

6dfac88947bda6b476b8a9d10861a621a279ebc3
Chris Ollenburg <chris.ollenburg@skookum.com>
2016-03-11 13:24:58 -0500

    Added snapshot helper test.

--- SnapshotTestUITests/SnapshotTestUITests.swift
+++ SnapshotTestUITests/SnapshotTestUITests.swift
@@ -17,10 +17,10 @@ class SnapshotTestUITests: XCTestCase {
         
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
     }
     
     override func tearDown() {
@@ -29,8 +29,8 @@ class SnapshotTestUITests: XCTestCase {
     }
     
     func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCUIApplication().childrenMatchingType(.Window).elementBoundByIndex(0).tap()
+        snapshot("01LoginScreen")
     }
     
 }
